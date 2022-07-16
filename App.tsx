58299a1da9cbb8b9e4cf01bad474b7880f5e1a21
Dionescha <dioneluisschallenberger@hotmail.com>
2022-07-15 22:20:50 -0300

    a

--- App.tsx
+++ App.tsx
@@ -1,14 +1,96 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
+  const [text, onChangeText] = useState('');
+  const [result, onChangeResult] = useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Header/>
+      
+      <View style={styles.container}>
+        <Input text={text} onChangeText={onChangeText}/>
+        <Button title='Calcular' onPress={() => {
+          Calcular({text, onChangeResult});
+          Subtrair({text, onChangeResult});
+          Multiplicar({text, onChangeResult});
+          Dividir({text, onChangeResult});
+        }}/>
+        
+        <Text style={styles.textResult}>
+          Resultado: 
+          <Text style={{paddingLeft: 8, fontWeight: '700'}}>{result}</Text>
+        </Text>
+
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
+
+function Header() {
+  return (
+    <View style={styles.headerBox}>
+      <Text style={styles.headerText}>App Matemática</Text>
+    </View>
+  );
+}
+
+function Input({text, onChangeText}) {
+  return (
+    <View style={{width: '100%'}}>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder={'Informe seu cálculo'}
+      />
+    </View>
+  )
+}
+
+function Calcular({text, onChangeResult}) {
+  const valores: string[] = text.split('+');
+  const resultado: number = Number(valores[0]) + Number(valores[1]);
+
+  if (resultado) {
+    onChangeResult(resultado);
+  }
+  
+}
+
+
+function Multiplicar({text, onChangeResult}) {
+  const valores: string[] = text.split('*');
+  const resultado: number = Number(valores[0]) * Number(valores[1]);
+
+  if (resultado) {
+    onChangeResult(resultado);
+  }
+  
+}
+
+function Subtrair({text, onChangeResult}) {
+  const valores: string[] = text.split('-');
+  const resultado: number = Number(valores[0]) - Number(valores[1]);
+
+  if (resultado) {
+    onChangeResult(resultado);
+  }
+  
+}
+function Dividir({text, onChangeResult}) {
+  const valores: string[] = text.split('/');
+  const resultado: number = Number(valores[0]) / Number(valores[1]);
+
+  if (resultado) {
+    onChangeResult(resultado);
+  }
+  
+}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +99,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerBox: {
+    height: 60,
+    backgroundColor: '#4867D7',
+  },
+  headerText: {
+    padding: 15,
+    fontSize: 20,
+    color: '#fff'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    borderWidth: 1
+  },
+  textResult: {
+    paddingTop: 40,
+    fontSize: 20
+  }
 });
